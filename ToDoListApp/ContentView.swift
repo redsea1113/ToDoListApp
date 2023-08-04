efd1d03a6f09dbf2fa3222088c407b6556e324f8
redsea1113 <tfdc2003@gmail.com>
2023-08-04 09:28:42 -0500

    Second Commit -  List Completed

--- ToDoListApp/ContentView.swift
+++ ToDoListApp/ContentView.swift
@@ -11,15 +11,30 @@ struct ContentView: View {
     
     @State var todos = [
         
-        Todo(title: "Study Unit 8"),
+        Todo(title: "Study Unit 8", subtitle: "I need it for the next class", isCompleted: true),
         Todo(title: "Finish all assignments"),
         Todo(title: "Play sports"),
         Todo(title: "See my friends")
     ]
     var body: some View {
         NavigationStack {
-            List(todos) { todo in
-                Text(todo.title)
+            List($todos) { $todo in
+                HStack{
+                    Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle") .onTapGesture {
+                        todo.isCompleted.toggle()
+                    }
+                    VStack(alignment: .leading) {
+                        Text(todo.title)
+                            .strikethrough(todo.isCompleted)
+                        if !todo.subtitle.isEmpty {
+                            Text(todo.subtitle)
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                                .strikethrough(todo.isCompleted)
+                            
+                        }
+                    }
+                }
             }.navigationTitle("My Todo List")
         }
         
