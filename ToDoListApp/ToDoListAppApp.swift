6864e1a1765ed28501be23711f5bc03a3d9dc93a
redsea1113 <tfdc2003@gmail.com>
2023-08-04 08:41:18 -0500

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Ali Farhat on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
